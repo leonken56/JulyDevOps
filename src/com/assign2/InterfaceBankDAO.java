0037afb45f1fdf68b6ae66359ace27e3822e7953
Anh Pham <anhpham.tech@gmail.com>
2021-07-15 19:35:21 -0700

    Convert you bank application into DAO design patterns Create a DAO class with Arraylist object to add all users
    Add the following methods in DAO class:
    - Add New User
    - Update user's Name
    - Update user's Email
    - Update user's Phone Number
    - Delete a user by Account Number
    - Display all users
    - Display user's info by selecting the name
    
    - Deposit Money - Don't allow user to deposit less than $5 and more than
    $10000
    
    - Display account balance

--- src/com/assign2/InterfaceBankDAO.java
+++ src/com/assign2/InterfaceBankDAO.java
@@ -1,12 +1,12 @@
 package com.assign2;
 import java.util.List;
-public interface BankDAO {
+public interface InterfaceBankDAO {
 	   public List<Bank> getAllAccounts();
 	   public Bank getAccount(String accountnumber);
 	   public void updateName(Bank user, String name);
 	   public void updateEmail(Bank user, String email);
 	   public void updatePhoneNumber(Bank user, String PhoneNumber);
-	   public void deleteAccount(Bank account);
+	   public void deleteAccount(String accountnumber);
 	   public void addNewAccount(Bank account);
 	   public void deposit(Bank account, double amount);
 	   public void displaybalance(Bank account);
